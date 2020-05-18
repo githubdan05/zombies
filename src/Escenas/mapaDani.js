1124e49fdeae95ee4e778320df2862768f2d9791
githubdan05 <olmocoto.b@gmail.com>
2020-05-18 18:48:45 +0200

    version minotauro

--- src/Escenas/mapaDani.js
+++ src/Escenas/mapaDani.js
@@ -25,11 +25,14 @@ export class MapaDani extends Phaser.Scene {
         // Cargamos el JSON del mapa
         this.load.tilemapTiledJSON('mapa', './assets/dani/mapa.json');
         this.load.spritesheet("tronco", "assets/log.png", { frameWidth: 32, frameHeight: 32 })
+        this.load.spritesheet("minotauro", "assets/minotaur.png", { frameWidth: 48, frameHeight: 48 })
+        this.load.spritesheet("soldado", "assets/Axeman Sprite.png", { frameWidth: 35.31, frameHeight: 41.25 })
         this.load.spritesheet("araña", "assets/spider01.png", { frameWidth: 64, frameHeight: 64 })
         this.load.spritesheet("bomba", "assets/bombs.png", { frameWidth: 16, frameHeight: 16 })
         this.load.spritesheet("explosion", "assets/explosion.png", { frameWidth: 400, frameHeight: 400 })
         this.load.spritesheet('player2', 'assets/character.png',{ frameWidth: 16, frameHeight: 32 })
         this.load.spritesheet('zombie', 'assets/dani.png',{ frameWidth: 16, frameHeight: 32 })
+        this.load.spritesheet('mazazoP', 'assets/minotaur(3).png',{ frameWidth: 48, frameHeight: 53 })
     }
 
     create() {
@@ -51,11 +54,15 @@ export class MapaDani extends Phaser.Scene {
         this.player1 = this.physics.add.sprite(150, 100, 'tronco');
         this.player1.body.setSize(10,10)
         this.zombiesM=0
+        let velocidadMinotauro=15
+        this.golpeMinotauro=false
         this.disparos=this.physics.add.group();
         this.zombies=this.physics.add.group();
         this.arañas=this.physics.add.group();
         this.vidas=this.physics.add.group();
         this.municiones=this.physics.add.group();
+        this.soldados=this.physics.add.group();
+        this.minotauros=this.physics.add.group();
         this.cañoncetes=this.physics.add.group();
         this.zapatillas=this.physics.add.group();
         this.balasDobles=this.physics.add.group();
@@ -81,12 +88,13 @@ export class MapaDani extends Phaser.Scene {
         this.player2vidas = this.add.text(390,39, 'vidas2: 20', { fontSize: '12px', fill: '#ffffff' });
         this.zombiesMT = this.add.text(200,16, 'zombies muertos: 0', { fontSize: '12px', fill: '#ffffff' });
         this.finDelJuego
+        this.torretas=this.physics.add.staticGroup()
         setInterval(()=>{
             let araña=this.arañas.create(50,50,"araña",1,true,true)
             araña.body.setSize(16,16)
             araña.anims.play('downA',true)
             araña.vida=3
-        },5000)
+        },25000)
         setInterval(()=>{
             let zombie=this.zombies.create(480,Phaser.Math.Between(430,450),"zombie",1,true,true)
             zombie.body.setSize(16,16)
@@ -268,6 +276,14 @@ export class MapaDani extends Phaser.Scene {
                 this.physics.pause()
                 this.add.text(100, 150, 'TE HAN COMIDO\n LOS ZOMBIES', { fontSize: '40px', fill: '#000000',backgroundColor:'#ffffff'});
             }
+            if(this.vidas2<=5){
+                setInterval(()=>{
+                    let soldado=this.soldados.create(this.player2.x,this.player.y,"soldado",1,true,true)
+                    soldado.body.setSize(16,16)
+                    soldado.anims.play('soldado',true)
+                    soldado.vida=3
+                },1000)
+            }
         })
         this.physics.add.collider(this.arañas,this.player2,(araña,player2)=>{
             this.vidas2=this.vidas2-1
@@ -282,26 +298,57 @@ export class MapaDani extends Phaser.Scene {
                 this.add.text(100, 150, 'TE HAN COMIDO\n LOS ZOMBIES', { fontSize: '40px', fill: '#000000',backgroundColor:'#ffffff'});
             }
         })
-        this.physics.add.collider(this.arañas,this.torreta,(araña,torreta)=>{
-            torreta.vidas=torreta.vidas-1
-            console.log('torreta.vidas')
-            this.mordiscoAraña=true
-            setTimeout(()=>{
-                this.mordiscoAraña=false
-            },1000)
-            if(torreta.vidas==0){
-               torreta.destroy()
-               this.torreta=null
+        
+        this.physics.add.collider(this.minotauros,this.player2,(minotauro,player2)=>{
+            this.vidas2=this.vidas2-1
+            this.player2vidas.setText('vidas2= '+this.vidas2)
+            this.golpeMinotauro=true
+            if(minotauro,player2){
+                this.Gminotauro=this.physics.add.sprite(minotauro.x,minotauro.y,'mazazo',0).setScale(1.5)
+                this.Gminotauro.anims.play('mazazo',true)
+            }
+            
+            if(this.vidas2==0){
+                this.physics.pause()
+                this.add.text(100, 150, 'TE HAN COMIDO\n LOS ZOMBIES', { fontSize: '40px', fill: '#000000',backgroundColor:'#ffffff'});
             }
         })
+        this.physics.add.collider(this.minotauros,this.player1,(minotauro,player1)=>{
+            this.vidas1=this.vidas1-1
+            this.player1vidas.setText('vidas1= '+this.vidas1)
+            this.golpeMinotauro=true
+            if(minotauro,player1){
+                //his.Gminotauro=this.physics.add.sprite(minotauro.x,minotauro.y,'mazazoP',0).setScale(1.5)
+                minotauro.anims.play('mazazo',true)
+            }
+            
+            if(this.vidas1==0){
+                this.physics.pause()
+                this.add.text(100, 150, 'TE HAN COMIDO\n LOS ZOMBIES', { fontSize: '40px', fill: '#000000',backgroundColor:'#ffffff'});
+            }
+        })
+        this.physics.add.collider(this.arañas,this.zombies)
+        this.physics.add.collider(this.zombies,this.minotauros)
+        this.physics.add.collider(this.arañas,this.minotauros)
         this.physics.add.collider(this.player1,this.player2)
         this.physics.add.collider(capaArbustos,this.player1)
         this.physics.add.collider(capaArbustos,this.player2)
         this.physics.add.collider(capaArbustos,this.zombies)
         this.physics.add.collider(capaArbustos,this.arañas)
+        this.physics.add.collider(capaArbustos,this.minotauros)
         this.physics.add.collider(capaSetos,this.player1)
         this.physics.add.collider(capaSetos,this.player2)
         this.physics.add.collider(capaSetos,this.zombies)
+        this.physics.add.collider(this.disparos,this.minotauros,(disparo,minotauro)=>{
+            disparo.destroy(true)
+            if(minotauro.vida==1){
+                minotauro.destroy(true)
+                this.zombiesM=this.zombiesM+1
+                this.zombiesMT.setText('zombies muertos: '+this.zombiesM)
+            }else{
+                minotauro.vida=minotauro.vida-1
+            }
+        })
         this.physics.add.collider(this.disparos,this.arañas,(disparo,araña)=>{
             disparo.destroy(true)
             if(araña.vida==1){
@@ -325,6 +372,12 @@ export class MapaDani extends Phaser.Scene {
             if(this.zombiesM%5==0 && this.vidasZombies!=5){
                 this.vidasZombies=this.vidasZombies+1
             }
+            if(this.zombiesM%1==0){
+                let minotauro=this.minotauros.create(480,Phaser.Math.Between(430,450),"mazazoP",1,true,true)
+                minotauro.body.setSize(16,16)
+                minotauro.scale=1.50
+                minotauro.vida=7
+            }
         })
         this.physics.add.collider(this.disparos,capaSetos,(disparo,setos)=>{
             this.zombieMM.play()
@@ -333,6 +386,18 @@ export class MapaDani extends Phaser.Scene {
         capaArbustos.setCollisionByProperty({collides:true})
         capaSetos.setCollisionByProperty({collides:true})
         this.player1.setCollideWorldBounds(true);
+        this.anims.create({
+            key: 'soldado',
+            frames: this.anims.generateFrameNumbers('soldado', { start: 0, end: 29 }),
+            frameRate: 10,
+            repeat: 0
+        });
+        this.anims.create({
+            key: 'mazazo',
+            frames: this.anims.generateFrameNumbers('minotaur(3)', { start: 0, end: 9 }),
+            frameRate: 10,
+            repeat: 0
+        });
         this.anims.create({
             key: 'explosion',
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 15 }),
@@ -418,7 +483,7 @@ export class MapaDani extends Phaser.Scene {
         this.keyS = this.input.keyboard.addKey('S');
         this.keyD = this.input.keyboard.addKey('D');
         this.keyQ = this.input.keyboard.addKey('Q');
-
+        this.keyZ = this.input.keyboard.addKey('Z');
 
         this.add.text(200, 30, 'Dani´s Village', { fontFamily: '"Roboto Condensed"', fontSize: 30 });
     }
@@ -431,9 +496,22 @@ export class MapaDani extends Phaser.Scene {
     update() {
         if (Phaser.Input.Keyboard.JustDown(this.keyQ)){
             if(this.torreta==null){
-                //this.torreta=this.add.image(this.player2.x,this.player2.y,'torreta').setScale(0.5)
-                this.torreta = this.physics.add.sprite(this.player2.x,this.player2.y,'torreta',0).setScale(0.5)
-                this.torreta.vidas=5
+                
+                this.torreta =this.physics.add.staticImage(this.player2.x,this.player2.y,'torreta',0).setSize(32,32).setScale(0.5)
+                this.torreta.vida=5
+                this.physics.add.collider(this.arañas,this.torreta,(araña,torreta)=>{
+                    this.torreta.vida=this.torreta.vida-1
+                    console.log(this.torreta.vida)
+                    this.mordiscoAraña=true
+                    setTimeout(()=>{
+                        this.mordiscoAraña=false
+                    },1000)
+                    if(this.torreta.vida==0){
+                       this.torreta.destroy()
+                       this.torreta=null
+                       console.log('e')
+                    }
+                })
             }
         }
         if (Phaser.Input.Keyboard.JustDown(this.keyP)){
@@ -442,7 +520,21 @@ export class MapaDani extends Phaser.Scene {
             }else{
                 this.explosion=this.physics.add.sprite(this.bomba.x,this.bomba.y,'explosion',0).setScale(0.3)
                 this.bomba.destroy()
+                this.bomba=null
                 this.explosion.anims.play('explosion',true)
+                this.zombies.children.iterate((zombie)=>{
+                    if(zombie){
+                        let distanciaZombie=Phaser.Math.Distance.Between(zombie.x,zombie.y,this.explosion.x,this.explosion.y)
+                        if(distanciaZombie<=200){
+                            zombie.destroy()
+                        }
+                    }
+                   
+                })
+                this.explosion.on('animationcomplete',()=>{
+                    this.explosion.destroy()
+                })
+                
             }
         }
         if (this.cursors.left.isDown || this.cursors.right.isDown || this.cursors.up.isDown || this.cursors.down.isDown) {
@@ -515,6 +607,20 @@ export class MapaDani extends Phaser.Scene {
             this.player2.setVelocityY(0);
             this.player2.anims.play('stopp2', true);
         }
+        this.minotauros.children.iterate((minotauro)=>{
+            let distanciaplayer1m=Phaser.Math.Distance.Between(this.player1.x,this.player1.y,minotauro.x,minotauro.y)
+            let distanciaplayer2m=Phaser.Math.Distance.Between(this.player2.x,this.player2.y,minotauro.x,minotauro.y)
+            if(this.golpeMinotauro==true){
+                this.physics.moveToObject(minotauro,this.player1,0) 
+            }
+            let velocidadMinotauro=15
+            if (distanciaplayer1m<distanciaplayer2m){
+                this.physics.moveToObject(minotauro,this.player1,velocidadMinotauro) 
+            }
+            else{
+                this.physics.moveToObject(minotauro,this.player2,velocidadMinotauro)
+            }
+        })
         this.arañas.children.iterate((araña)=>{
             let distanciaplayer1a=Phaser.Math.Distance.Between(this.player1.x,this.player1.y,araña.x,araña.y)
             let distanciaplayer2a=Phaser.Math.Distance.Between(this.player2.x,this.player2.y,araña.x,araña.y)
